5f685a15d1a4aba49e8d4e652975cdaa29483712
CoolDevFor <coolfor42@gmail.com>
2021-12-14 23:01:15 +0900

    change port heroku

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import http from "http";
  */
 
 const env = process.env.NODE_ENV || 'development';
-var port = config.server[env].port;
+var port = config.server[env].port || process.env.port;
 app.set("port", port);
 
 /**
